b0158a5ea976b2991c4d41e5006973fd4633465e
FranciscoBarboza <francisco.barboza.tomas@gmail.com>
2022-08-27 01:42:59 -0300

    agrego las estructuras y empiezo a agregarlas

--- TP-1/Vista/ejercicio_1.php
+++ TP-1/Vista/ejercicio_1.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Ejercicio 1</title>
+<?php
+include_once "../../cabecera/cabecera-php.php"
+?> 
+
+
     <script src="bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.min.css">
-</head>
-<body>
+
+
+
+
     <form action="../Control/ejercicio_1.php" class="row g-3 needs-validation" novalidate method="get">
         <div class="col-md-4 m-4">
             <input type="number" class="form-control" name="input_numero" id="input_numero" required>
@@ -41,5 +43,7 @@
     })
     })()
   </script>
-</body>
-</html>
+<link rel="stylesheet" href="">
+<?php
+    include_once "../../cabecera/footer-php.php"
+?>
---
+++ cabecera/footer-php.php
@@ -0,0 +1,21 @@
+
+        <link rel="stylesheet" href="../../cabecera/footer-css.css">
+    
+
+    <div id="pie">
+        <div id="info">
+            <span>
+                universidad nacional del comahue
+            </span>
+        </div>
+        <div id="nombres">
+            <span>Francisco tomas Barboza -FAI-3595</span><br>
+            <span>Jorge Segura - FAI-231</span><br>
+            <span>Juan Manuel Fernandez Martin - FAI-3641</span><br>
+            <span>Legnazzi Sebastian Nicolas - FAI-3152</span>
+        </div>
+    </div>
+        
+
+    </body>
+</html>
